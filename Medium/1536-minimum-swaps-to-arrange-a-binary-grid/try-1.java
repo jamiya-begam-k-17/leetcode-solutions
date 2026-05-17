3aa7728f6b0c0339eb12a165859d7592ca2f5403
JAMIYA BEGAM K <149665054+jamiya-begam-k-17@users.noreply.github.com>
2026-05-17 12:31:50 +0530

    Add Medium/1536-minimum-swaps-to-arrange-a-binary-grid/try-1.java

---
+++ Medium/1536-minimum-swaps-to-arrange-a-binary-grid/try-1.java
@@ -0,0 +1,52 @@
+/*
+ * Problem #1536: Minimum Swaps to Arrange a Binary Grid
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: java
+ * Date: 3/2/2026, 6:06:51 AM
+ * Link: https://leetcode.com/problems/minimum-swaps-to-arrange-a-binary-grid/
+ */
+
+class Solution {
+    public int minSwaps(int[][] grid) {
+        int n = grid.length;
+        int[] conseq0 = new int[n]; // Count trailing zeros for all rows
+        for (int i = 0; i < n; ++i)
+        {
+            int counter = 0;
+            for (int j = n - 1; j >= 0; --j)
+            {
+                if (grid[i][j] != 0)
+                    break;
+                counter++;
+            }
+            conseq0[i] = counter;
+        }
+        int numNeeded = n - 1;
+        int ans = 0;
+        while (numNeeded > 0)
+        {
+            boolean flag = false;
+            for (int i = n - numNeeded - 1; i < n; ++i)
+            {
+                if (conseq0[i] >= numNeeded)
+                {
+                    //Perform the swaps
+                    int temp = conseq0[i];
+                    for (int k = i; k > n - numNeeded - 1; --k)
+                        conseq0[k] = conseq0[k - 1];
+                    conseq0[n - numNeeded - 1] = temp;
+                    flag = true;
+                    //Calculate the number of swaps
+                    ans += i - (n - numNeeded - 1);
+                    break;
+                }
+            }
+            if (!flag)
+                return -1;
+            numNeeded--;
+        }
+        return ans;
+    }
+}
