2b21bd94ab0717c5824b8ff5afe1c0a3fd049a33
JAMIYA BEGAM K <149665054+jamiya-begam-k-17@users.noreply.github.com>
2026-05-17 12:38:17 +0530

    Add Hard/2141-maximum-running-time-of-n-computers/try-1.java

---
+++ Hard/2141-maximum-running-time-of-n-computers/try-1.java
@@ -0,0 +1,45 @@
+/*
+ * Problem #2141: Maximum Running Time of N Computers
+ * Difficulty: Hard
+ * Submission: Try 1
+ * status: Accepted
+ * Language: java
+ * Date: 12/1/2025, 10:16:25 PM
+ * Link: https://leetcode.com/problems/maximum-running-time-of-n-computers/
+ */
+
+class Solution {
+    public long maxRunTime(int n, int[] batteries) {
+        long right = 0, left = 0;
+
+        for (int b : batteries) {
+            right += b;
+        }
+        right /= n;
+
+        while (left < right) {
+            long mid = (left + right + 1) / 2;
+
+            if (check(n, batteries, mid)) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+
+    public boolean check(int n, int[] batteries, long T) {
+        long total = 0;
+
+        for (int b : batteries) {
+            total += Math.min(b, T);
+            if (total >= (long) n * T) {
+                return true;
+            }
+        }
+
+        return total >= (long) n * T;
+    }
+}
