87bc446b3a4cd24235949fe788eb23d7a2bd18a6
JAMIYA BEGAM K <149665054+jamiya-begam-k-17@users.noreply.github.com>
2026-05-17 12:34:48 +0530

    Add Easy/66-plus-one/try-3.java

---
+++ Easy/66-plus-one/try-3.java
@@ -0,0 +1,27 @@
+/*
+ * Problem #66: Plus One
+ * Difficulty: Easy
+ * Submission: Try 3
+ * status: Accepted
+ * Language: java
+ * Date: 1/1/2026, 3:20:23 PM
+ * Link: https://leetcode.com/problems/plus-one/
+ */
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for(int i=n-1;i>=0;i--) {
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            } 
+            digits[i]=0;
+        }
+
+        digits = new int[n+1];
+        digits[0]=1;
+        return digits;
+    }
+}
+
