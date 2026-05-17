6880443bd5dc09d44fc16f1203f395c1cee54109
JAMIYA BEGAM K <149665054+jamiya-begam-k-17@users.noreply.github.com>
2026-05-17 12:32:31 +0530

    Add Easy/693-binary-number-with-alternating-bits/try-1.java

---
+++ Easy/693-binary-number-with-alternating-bits/try-1.java
@@ -0,0 +1,22 @@
+/*
+ * Problem #693: Binary Number with Alternating Bits
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: java
+ * Date: 2/18/2026, 7:48:43 PM
+ * Link: https://leetcode.com/problems/binary-number-with-alternating-bits/
+ */
+
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        int cur = n % 2;
+        n /= 2;
+        while (n > 0) {
+            if (cur == n % 2) return false;
+            cur = n % 2;
+            n /= 2;
+        }
+        return true;
+    }
+}
