05fc4e98845827def2ba1d87603714b49f7c2741
JAMIYA BEGAM K <149665054+jamiya-begam-k-17@users.noreply.github.com>
2026-05-17 12:35:42 +0530

    Add Medium/3652-best-time-to-buy-and-sell-stock-using-strategy/try-1.java

---
+++ Medium/3652-best-time-to-buy-and-sell-stock-using-strategy/try-1.java
@@ -0,0 +1,41 @@
+/*
+ * Problem #3652: Best Time to Buy and Sell Stock using Strategy
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: java
+ * Date: 12/18/2025, 7:21:18 PM
+ * Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-using-strategy/
+ */
+
+class Solution {
+    public long maxProfit(int[] prices, int[] strategy, int k) {
+        int n = prices.length;
+
+        long base_profit = 0;
+        for (int i = 0; i < n; i++) {
+            base_profit += strategy[i] * prices[i];
+        }
+
+        long[] prefix_sum_prices = new long[n + 1];
+        long[] prefix_sum_strategy = new long[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            prefix_sum_prices[i + 1] = prefix_sum_prices[i] + prices[i];
+            prefix_sum_strategy[i + 1] = prefix_sum_strategy[i] + strategy[i] * prices[i];
+        }
+
+        long max_gain = 0;
+        int half = k / 2;
+
+        for (int i = 0; i + k <= n; i++) {
+            long old_segment = prefix_sum_strategy[i + k] - prefix_sum_strategy[i];
+            long new_segment = prefix_sum_prices[i + k] - prefix_sum_prices[i + half];
+
+            long gain = new_segment - old_segment;
+            max_gain = Math.max(max_gain, gain);
+        }
+
+        return base_profit + Math.max(0, max_gain);
+    }
+}
