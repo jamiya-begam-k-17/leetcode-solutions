d924b348881ccc0016b6d847ce12828a72f6e6b5
JAMIYA BEGAM K <149665054+jamiya-begam-k-17@users.noreply.github.com>
2026-05-17 12:32:35 +0530

    Add Easy/190-reverse-bits/try-2.java

---
+++ Easy/190-reverse-bits/try-2.java
@@ -0,0 +1,25 @@
+/*
+ * Problem #190: Reverse Bits
+ * Difficulty: Easy
+ * Submission: Try 2
+ * status: Accepted
+ * Language: java
+ * Date: 2/16/2026, 10:16:16 PM
+ * Link: https://leetcode.com/problems/reverse-bits/
+ */
+
+class Solution {
+    public int reverseBits(int n) { return rev(n, 32); }
+
+    private int rev(int v, int len) {
+        if (len == 1) return v & 1;
+
+        int half = len >> 1;
+        int mask = (1 << half) - 1;
+        int lo = v & mask;
+        int hi = v >>> half;
+
+        return (rev(lo, half) << half) | rev(hi, half);
+    }
+}
+
