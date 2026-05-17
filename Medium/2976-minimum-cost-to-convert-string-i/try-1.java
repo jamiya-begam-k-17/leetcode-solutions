99055ecae7fc589db8ea4723eedaa2c46549f409
JAMIYA BEGAM K <149665054+jamiya-begam-k-17@users.noreply.github.com>
2026-05-17 12:34:21 +0530

    Add Medium/2976-minimum-cost-to-convert-string-i/try-1.java

---
+++ Medium/2976-minimum-cost-to-convert-string-i/try-1.java
@@ -0,0 +1,60 @@
+/*
+ * Problem #2976: Minimum Cost to Convert String I
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: java
+ * Date: 1/29/2026, 9:45:28 AM
+ * Link: https://leetcode.com/problems/minimum-cost-to-convert-string-i/
+ */
+
+class Solution {
+
+    public long minimumCost (
+        String source,
+        String target,
+        char[] original,
+        char[] changed,
+        int[] cost
+    ) {
+        long totalCost = 0;
+        long[][] minCost = new long[26][26];
+        for(long[] row: minCost) {
+            Arrays.fill(row, Integer.MAX_VALUE);
+        }
+
+        for(int i=0;i<original.length;i++) {
+            int startChar = original[i]-'a';
+            int endChar = changed[i]-'a';
+            minCost[startChar][endChar]=Math.min(
+                minCost[startChar][endChar],
+                (long)cost[i]
+                );
+        }
+
+        for(int k=0;k<26;k++) {
+            for(int i=0;i<26;i++) {
+                for(int j=0;j<26;j++) {
+                    minCost[i][j]=Math.min(
+                        minCost[i][j],
+                        minCost[i][k] + minCost[k][j]
+                    );
+                }
+            }
+        }
+
+        for(int i=0;i<source.length();i++) {
+            if(source.charAt(i)==target.charAt(i)) {
+                continue;
+            }
+            int sourceChar = source.charAt(i)-'a';
+            int targetChar = target.charAt(i)-'a';
+
+            if(minCost[sourceChar][targetChar]>=Integer.MAX_VALUE) {
+                return -1;
+            }
+            totalCost+=minCost[sourceChar][targetChar];
+        }
+        return totalCost;
+    }
+}
