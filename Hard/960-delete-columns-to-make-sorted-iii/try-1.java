a0b18b73fa572aaba85089138fd6d0f5af7972aa
JAMIYA BEGAM K <149665054+jamiya-begam-k-17@users.noreply.github.com>
2026-05-17 12:35:25 +0530

    Add Hard/960-delete-columns-to-make-sorted-iii/try-1.java

---
+++ Hard/960-delete-columns-to-make-sorted-iii/try-1.java
@@ -0,0 +1,30 @@
+/*
+ * Problem #960: Delete Columns to Make Sorted III
+ * Difficulty: Hard
+ * Submission: Try 1
+ * status: Accepted
+ * Language: java
+ * Date: 12/22/2025, 10:04:42 PM
+ * Link: https://leetcode.com/problems/delete-columns-to-make-sorted-iii/
+ */
+
+class Solution {
+    public int minDeletionSize(String[] A) {
+        int W = A[0].length();
+        int[] dp = new int[W];
+        Arrays.fill(dp, 1);
+        for (int i = W-2; i >= 0; --i)
+            search: for (int j = i+1; j < W; ++j) {
+                for (String row: A)
+                    if (row.charAt(i) > row.charAt(j))
+                        continue search;
+
+                dp[i] = Math.max(dp[i], 1 + dp[j]);
+            }
+
+        int kept = 0;
+        for (int x: dp)
+            kept = Math.max(kept, x);
+        return W - kept;
+    }
+}
