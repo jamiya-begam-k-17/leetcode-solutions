d351a9bca4ebeb2a59cd2f812b812bea85f649d1
JAMIYA BEGAM K <149665054+jamiya-begam-k-17@users.noreply.github.com>
2026-05-25 23:52:53 +0530

    Add Medium/189-rotate-array/try-2.java

---
+++ Medium/189-rotate-array/try-2.java
@@ -0,0 +1,30 @@
+/*
+ * Problem #189: Rotate Array
+ * Difficulty: Medium
+ * Submission: Try 2
+ * status: Accepted
+ * Language: java
+ * Date: 5/20/2026, 12:24:41 PM
+ * Link: https://leetcode.com/problems/rotate-array/
+ */
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k=k%n;
+        reverse(nums,n-k,n-1);
+        reverse(nums,0,n-k-1);
+        reverse(nums,0,n-1);
+        
+    }
+    public void reverse(int[] nums, int l, int r) {
+        while(l<r) {
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+
+            l++;
+            r--;
+        }
+    }
+}
