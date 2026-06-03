e6584100be74e45ba7d91416d75e61411217a0ae
JAMIYA BEGAM K <149665054+jamiya-begam-k-17@users.noreply.github.com>
2026-06-03 19:49:26 +0530

    Add Medium/54-spiral-matrix/try-3.java

---
+++ Medium/54-spiral-matrix/try-3.java
@@ -0,0 +1,43 @@
+/*
+ * Problem #54: Spiral Matrix
+ * Difficulty: Medium
+ * Submission: Try 3
+ * status: Accepted
+ * Language: java
+ * Date: 5/26/2026, 12:29:55 PM
+ * Link: https://leetcode.com/problems/spiral-matrix/
+ */
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        List<Integer> list = new ArrayList<>();
+        int top = 0, right = m-1, bottom = n-1, left = 0;
+
+        while(top<=bottom && left<=right) {
+            for(int i=left;i<=right;i++) {
+                list.add(matrix[left][i]);
+            }
+            top++;
+            for(int i=top;i<=bottom;i++) {
+                list.add(matrix[i][right]);
+            }
+            right--;
+            if(top<=bottom) {
+                for(int i=right;i>=left;i--) {
+                    list.add(matrix[bottom][i]);
+                }
+            bottom--;
+            }
+            if(left<=right) {
+                for(int i=bottom;i>=top;i--) {
+                    list.add(matrix[i][left]);
+                }
+            left++;
+            }
+        }
+
+        return list;
+    }
+}
